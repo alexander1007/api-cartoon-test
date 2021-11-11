11a3c57bdc665f3d76a0479ec5ac79f746e251e3
ESTRATEC\yhernandez <yenifer.hernandez@masivian.com>
2021-11-10 20:58:17 -0500

    configuracion cors

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: false,
 }));
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.get('/', (req, res) => {
   axios({
     url: `${url}/${req.body.cartoon}/info.0.json`,
