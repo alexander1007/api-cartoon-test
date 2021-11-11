69d93d44d9a76140d30a9147055fe4b129dfc576
ESTRATEC\yhernandez <yenifer.hernandez@masivian.com>
2021-11-10 20:04:10 -0500

    creacion y configuracion de proyecto api para cartoon-test

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const url = 'https://xkcd.com';
+
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false,
+}));
+app.get('/', (req, res) => {
+  axios({
+    url: `${url}/${req.body.comic}/info.0.json`,
+    method: 'get',
+  }).then((response) => {
+    res.json(response.data);
+  }).catch((err) => {
+    console.error(`The comic could not be consulted error => ${err}`);
+  });
+ 
+});
+
+app.listen(port, () => {
+  console.log(`start server http://localhost:${port}`)
+})
